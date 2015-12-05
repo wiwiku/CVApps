dfe39a6dc03241f3654a1b865b0ffbc75f802a36
Jaineel Dalal <dalaljaineel@gmail.com>
2015-12-04 22:58:30 -0500

    Added Real Time Hough to Canny Edge Detection. Needs tuning and video feed overlay

--- RealTime_Canny/RealTime_Canny/ViewController.m
+++ RealTime_Canny/RealTime_Canny/ViewController.m
@@ -43,29 +43,60 @@
     
     // Initialize all the required filters over here before Daisychaining them
     // Grayscale
-    /*GPUImageGrayscaleFilter *videoGrayFilter = [[GPUImageGrayscaleFilter alloc] init];
+    /*
+    GPUImageGrayscaleFilter *videoGrayFilter = [[GPUImageGrayscaleFilter alloc] init];
     //[videoGrayFilter forceProcessingAtSize:CGSizeMake(640.0, 480.0)];
     [videoGrayFilter forceProcessingAtSizeRespectingAspectRatio:CGSizeMake(640, 480)];
     
     // Gaussian blurring
     GPUImageGaussianBlurFilter *gaussianblurFilter = [[GPUImageGaussianBlurFilter alloc] init];
     gaussianblurFilter.blurRadiusInPixels = 3.5;
-    gaussianblurFilter.blurPasses = 1;*/
-    
-    // Canny Edge Detection
-    GPUImageCannyEdgeDetectionFilter *cannyEdgeFilter = [[GPUImageCannyEdgeDetectionFilter alloc] init];
-    cannyEdgeFilter.blurRadiusInPixels = 1.5;
-    cannyEdgeFilter.upperThreshold = 0.5;
-    cannyEdgeFilter.lowerThreshold = 0.2;
-    
-    // Hough Transform
-    
+    gaussianblurFilter.blurPasses = 1;
+     
     // Daisy chain all the filters together
     //[videoCamera addTarget:videoGrayFilter];
     //[videoGrayFilter addTarget:gaussianblurFilter];
     //[gaussianblurFilter addTarget:videoView_];
+    */
+    
+    // Canny Edge Detection
+    /*
+    GPUImageCannyEdgeDetectionFilter *cannyEdgeFilter = [[GPUImageCannyEdgeDetectionFilter alloc] init];
+    cannyEdgeFilter.blurRadiusInPixels = 1.5;
+    cannyEdgeFilter.upperThreshold = 0.5;
+    cannyEdgeFilter.lowerThreshold = 0.2;
+     
+    // Daisy Chain all the filters together
     [videoCamera addTarget:cannyEdgeFilter];
     [cannyEdgeFilter addTarget:videoView_];
+     */
+    
+    // Hough Transform
+    // Hough Lines
+    GPUImageHoughTransformLineDetector *lineDetector = [[GPUImageHoughTransformLineDetector alloc] init];
+    // lineDetector.edgeThreshold = 0.05f; // default = 0.0
+    lineDetector.lineDetectionThreshold = 0.175f; // default = 0.12
+     
+    GPUImageLineGenerator *lineGenerator = [[GPUImageLineGenerator alloc] init];
+    [lineGenerator setLineColorRed:1.0 green:0.0 blue:0.0];
+    [lineGenerator forceProcessingAtSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height)];
+     
+    [
+    lineDetector setLinesDetectedBlock:^(GLfloat* lineArray, NSUInteger linesDetected, CMTime frameTime)
+    {
+    NSLog(@"Number of lines: %ld", (unsigned long)linesDetected);
+    [lineGenerator renderLinesFromArray:lineArray count:linesDetected frameTime:frameTime];
+    }
+    ];
+    
+    /*GPUImageAlphaBlendFilter *blendFilter = [[GPUImageAlphaBlendFilter alloc] init];
+    [blendFilter forceProcessingAtSize:CGSizeMake(480.0, 640.0)];
+    */
+    
+    // Daisy chain the filters together (you can add as many filters as you like)
+    [videoCamera addTarget:lineDetector];
+    [lineDetector addTarget:lineGenerator];
+    [lineGenerator addTarget:videoView_];
     
     
     // Starting to capture

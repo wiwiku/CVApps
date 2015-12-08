8f98d1a68f58d0235709392c3f124baa0645fae5
William Ku <williamku@berkeley.edu>
2015-12-08 17:28:13 -0500

    Added movie file support in SimpleVideoFilter

--- SimpleVideoFilter/SimpleVideoFilter/SimpleVideoFilterViewController.m
+++ SimpleVideoFilter/SimpleVideoFilter/SimpleVideoFilterViewController.m
@@ -10,14 +10,6 @@
 
 @implementation SimpleVideoFilterViewController
 
-//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-//{
-//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-//    if (self) {
-//    }
-//    return self;
-//}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -30,14 +22,19 @@
     [super viewDidLoad];
 	
 	/* === Camera and frame setup === */
-    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
-//    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
-//    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1280x720 cameraPosition:AVCaptureDevicePositionBack];
-//    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1920x1080 cameraPosition:AVCaptureDevicePositionBack];
+//    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+//    videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+//	  videoCamera.runBenchmark = YES;
+	
+	/* === Use movie file === */
+	// Set the movie file to read
+    NSURL *sampleURL = [[NSBundle mainBundle] URLForResource:@"simon" withExtension:@"mov"];
+    
+    GPUImageMovie *movieFile = [[GPUImageMovie alloc] initWithURL:sampleURL];
+    movieFile.runBenchmark = YES;
+    movieFile.playAtActualSpeed = YES;
 
-    videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
-	videoCamera.runBenchmark = YES;
-
+	/* === Output view (screen) === */
 	GPUImageView *filterView = (GPUImageView *)self.view;
 
 	/* === Filter declarations === */
@@ -65,9 +62,9 @@
     // Daisy chain the filters together (you can add as many filters as you like)
 //	[videoCamera addTarget:thresholdFilter];
 
-    [videoCamera addTarget:houghDetector];
+    [movieFile addTarget:houghDetector];
 
-	[videoCamera addTarget:blendFilter1];
+	[movieFile addTarget:blendFilter1];
 	[lineGenerator addTarget:blendFilter1];
 
 	[blendFilter1 addTarget:blendFilter2];
@@ -77,7 +74,7 @@
 	
 	// Callback function for line detection
     [houghDetector setLinesDetectedBlock:^(GLfloat* lineArray, NSUInteger linesDetected, CMTime frameTime){
-		// Find left and right lanes
+		// Find lines within slope bound
 		float lowm = 0.5, highm = 0.65;
 		float leftcount = 0, leftm = 0, leftb = 0, rightcount = 0, rightm = 0, rightb = 0;
 		bool hasLeft = false, hasRight = false;
@@ -97,11 +94,10 @@
 				leftm += m;
 				leftb += b;
 				hasLeft = true;
-
 			}
 		}
-		
-		// Copy filtered lines to old array
+
+		// Compute average lines and store in old array
 		int nLines = 0;
 		float xInter = 0;
 		if (hasRight) {
@@ -123,14 +119,16 @@
 			nLines++;
 		}
 
+		// Debug prints
 		NSLog(@"Number of lines: %ld; Number of new lines: %d", (unsigned long) linesDetected, nLines);
-
 		for (int i = 0; i < nLines; i++) {
 			NSLog(@"%f %f %f %f", lineArray[0], lineArray[1], lineArray[2], lineArray[3]);
 		}
 
+		// Render lines
 		[lineGenerator renderLinesFromArray:lineArray count:nLines frameTime:frameTime];
-		
+
+		// Generate crosses
 		if (nLines == 0) xInter = 0.5;
 		else xInter /= nLines;
 		int nPoints = 20;
@@ -139,89 +137,10 @@
 			center[2*i] = xInter;
 			center[2*i+1] = 1.0 - 0.8 / nPoints * i;
 		}
+		
+		// Render crosses
 		[crosshairGenerator renderCrosshairsFromArray:center count:nPoints frameTime:frameTime];
     }];
-
-//	// Callback function for line detection
-//    [houghDetector setLinesDetectedBlock:^(GLfloat* lineArray, NSUInteger linesDetected, CMTime frameTime){
-//		// Filter lines with slope greater than threshold
-//		float thres = 1; // diagonal
-//		float cmpPt = 0.0;
-////		int nFiltered = [self filterLines:lineArray withLength:(int) linesDetected withThres:thres];
-//
-//		// Find left and right lines
-//		GLfloat lines[4];
-//		float centerx = 0.5;
-//		float leftmin = 10000, rightmin = 10000;
-//		bool hasLeft = false, hasRight = false;
-//		for (int i = 0; i < linesDetected; i++) {
-//			float m = lineArray[2*i], b = lineArray[2*i+1];
-//			NSLog(@"m: %f, b: %f", lineArray[2*i], lineArray[2*i+1]);
-//
-//			// Filter
-//			if (fabsf(m) < thres || fabsf(m) >= 3) continue;
-//
-//			float x = [self xInterceptAty:cmpPt m:m b:b];
-//			float d = centerx - x;//x - centerx;
-//			if (d >= 0) { // Right side
-////				NSLog(@"m: %f, b: %f, x: %f, rightd: %f", lineArray[2*i], lineArray[2*i+1], x, d);
-//				if (d < rightmin) {
-//					lines[0] = m;
-//					lines[1] = b;
-//					rightmin = d;
-//					hasRight = true;
-//				}
-//			} else { // Left side
-////				NSLog(@"m: %f, b: %f, x: %f, leftd: %f", lineArray[2*i], lineArray[2*i+1], x, -d);
-//				if (-d < leftmin) {
-//					lines[2] = m;
-//					lines[3] = b;
-//					leftmin = -d;
-//					hasLeft = true;
-//				}
-//			}
-//		}
-//
-//		// Copy filtered lines to old array
-//		int nLines = 0;
-//		float xInter = 0;
-////		float xr = 0, xl = 0;
-////		if (hasRight) {
-////			lineArray[0] = lines[0];
-////			lineArray[1] = lines[1];
-////			xInter += [self xInterceptAty:1 m:lines[0] b:lines[1]];
-////			xr = [self xInterceptAty:cmpPt m:lines[0] b:lines[1]];
-////			nLines++;
-////
-////			if (hasLeft) {
-////				lineArray[2] = lines[2];
-////				lineArray[3] = lines[3];
-////				xInter += [self xInterceptAty:1 m:lines[2] b:lines[3]];
-////				xl = [self xInterceptAty:cmpPt m:lines[2] b:lines[3]];
-////				nLines++;
-////			}
-////		} else if (hasLeft) {
-////			lineArray[0] = lines[2];
-////			lineArray[1] = lines[3];
-////			xInter += [self xInterceptAty:1 m:lines[2] b:lines[3]];
-////			xl = [self xInterceptAty:cmpPt m:lines[2] b:lines[3]];
-////			nLines++;
-////		}
-//
-//		NSLog(@"Number of lines: %ld; Number of new lines: %d", (unsigned long) linesDetected, nLines);
-//
-//		for (int i = 0; i < nLines; i++) {
-//			NSLog(@"%f %f %f %f", lines[0], lines[1], lines[2], lines[3]);
-//		}
-//
-//		[lineGenerator renderLinesFromArray:lineArray count:linesDetected frameTime:frameTime];
-//
-//		if (nLines == 0) xInter = 0.5;
-//		else xInter /= nLines;
-////		GLfloat center[] = {xr, cmpPt, xl, cmpPt, xInter, 1.0};
-//		GLfloat center[] = {0.5, 1.0};
-//		[crosshairGenerator renderCrosshairsFromArray:center count:1 frameTime:frameTime];
-//    }];
 
 	// Start camera
     [videoCamera startCameraCapture];
@@ -232,52 +151,10 @@
     [super viewDidUnload];
 }
 
+/* Calculates x-intercept of a line y=mx+b at a given y. */
 - (float)xInterceptAty:(float)y m:(float)m b:(float)b {
 	if (m >= 100000) return b / 2 + 0.5;
 	else return (y - b) / m / 2 + 0.5;
 }
-
-//- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
-//{
-//    // Map UIDeviceOrientation to UIInterfaceOrientation.
-//    UIInterfaceOrientation orient = UIInterfaceOrientationPortrait;
-//    switch ([[UIDevice currentDevice] orientation])
-//    {
-//        case UIDeviceOrientationLandscapeLeft:
-//            orient = UIInterfaceOrientationLandscapeLeft;
-//            break;
-//
-//        case UIDeviceOrientationLandscapeRight:
-//            orient = UIInterfaceOrientationLandscapeRight;
-//            break;
-//
-//        case UIDeviceOrientationPortrait:
-//            orient = UIInterfaceOrientationPortrait;
-//            break;
-//
-//        case UIDeviceOrientationPortraitUpsideDown:
-//            orient = UIInterfaceOrientationPortraitUpsideDown;
-//            break;
-//
-//        case UIDeviceOrientationFaceUp:
-//        case UIDeviceOrientationFaceDown:
-//        case UIDeviceOrientationUnknown:
-//            // When in doubt, stay the same.
-//            orient = fromInterfaceOrientation;
-//            break;
-//    }
-//    videoCamera.outputImageOrientation = orient;
-//
-//}
-
-//- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-//{
-//    return YES; // Support all orientations.
-//}
-
-//- (IBAction)updateSliderValue:(id)sender
-//{
-//    [(GPUImageSepiaFilter *)filter setIntensity:[(UISlider *)sender value]];
-//}
 
 @end
